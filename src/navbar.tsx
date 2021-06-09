6431ee943f5e87f8ecc0bf73d43ef52b036de5c4
Milosz Czapla <totmelek@gmail.com>
2021-06-09 22:57:38 +0200

    NavBar polished, It looked awful before

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -6,13 +6,15 @@ const alt = 'Drinker icon';
 const NavBar = () => {
   return (
     <nav className='nav'>
-      <Link className='nav-link' to='/'>
-        <img className='nav-link-img' src={icon} alt={alt} />
-        <h2>Home</h2>
-      </Link>
-      <Link className='nav-link' to='/about'>
-        <h2>About</h2>
-      </Link>
+      <section className='nav-section'>
+        <Link className='nav-link' to='/'>
+          <img className='nav-link-img' src={icon} alt={alt} />
+          <h2>Home</h2>
+        </Link>
+        <Link className='nav-link' to='/about'>
+          <h2>About</h2>
+        </Link>
+      </section>
     </nav>
   );
 };
