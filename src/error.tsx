36ad5228c6675a8fc94f89c5fb4c2f84a2a938ce
Milosz Czapla <totmelek@gmail.com>
2021-06-10 09:25:06 +0200

    Added Eroor page with style

--- src/error.tsx
+++ src/error.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import icon from './assets/img/drinkericon-large.svg';
+
 const Error = () => {
-  return <div></div>;
+  const alt = 'Drinker icon';
+  return (
+    <main className='error'>
+      <h1 className='error-title'>Error Ocured</h1>
+      <article className='error-mess'>
+        Something went wrong and you end up with error. Click the button below
+        to come back to Drinker home site
+      </article>
+      <Link className='error-link nav-link' to='/'>
+        <img className='nav-link-img' src={icon} alt={alt} />
+        <h2>Home</h2>
+      </Link>
+    </main>
+  );
 };
 
 export default Error;
