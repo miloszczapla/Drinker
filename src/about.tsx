5b0681262ac869d542802c8f9681c91ae293ab3b
Milosz Czapla <totmelek@gmail.com>
2021-06-10 10:07:43 +0200

    added style to error text

--- src/about.tsx
+++ src/about.tsx
@@ -1,5 +1,18 @@
 const About = () => {
-  return <div></div>;
+  return (
+    <main className='about'>
+      <article className='about-info'>
+        Drinker is a powerful search engine for the best drinks out there with
+        minimalistic look and rich information about every position. -At least I
+        wish it was
+      </article>
+      <article className='about-example'>
+        If you are looking for a drink with few exact igridient use this syntax:
+        <h4>Igridient I + space + Igrideint II + ....</h4>
+      </article>
+      <h1 className='about-banner'>Enjoy!</h1>
+    </main>
+  );
 };
 
 export default About;
