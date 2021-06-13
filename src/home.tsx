a6c36bd84f25f2945c9610ac835cbf6e79d1d5cc
Milosz Czapla <totmelek@gmail.com>
2021-06-13 19:13:18 +0200

    Fetching data and filtring is working now

--- src/home.tsx
+++ src/home.tsx
@@ -33,89 +33,101 @@ const Home = () => {
     strDrinkThumb: string;
   }
 
+  interface Data {
+    drinkArray: Drink[];
+    duplicateArray: Drink[];
+    hasError: boolean;
+  }
+
+  interface DataElement {
+    drinks: Drink[];
+  }
+
   const getDrinks = useCallback(
-    //Promise.all to fetch data from all posible urls
-    (phrases: string, endPoints: string[]): Data => {
+    async (phrases: string, endPoints: string[]): Promise<Data> => {
+      //Promise.all to fetch data from all posible urls
+      let seen = new Set();
+      let duplicateMap = new Map();
+
       let drinkArray: Drink[] = [];
       let errorArray: Object[] = [];
-      let allUlrs: string[];
+      let allUrls: string[][] = [];
       const phrasesArray = phrases.split(',');
 
       phrasesArray.forEach((phrase) => {
-        // let filtredData: Drink[] = [];
-        endPoints.forEach((link) => {
-          allUlrs.push(link + phrase);
-        }); // delete in case of backing
-      }); // delete in case of backing
+        const arrayOfUrls: string[] = endPoints.map(
+          (endPoint) => endPoint + phrase
+        );
+        allUrls.push(arrayOfUrls);
+      });
 
-      //     fetch(link + phrase)
-      //       .then((response) => response.json())
-      //       .then((data: any) => {
-      //         const seen = new Set();
+      const response = allUrls.map(async (urlList) => {
+        let seenDuplicate = new Set();
+        let filteredData: Drink[] = [];
+        await Promise.all(urlList.map((url) => fetch(url)))
+          .then((response) => {
+            return Promise.all(
+              response.map((response) => {
+                return response.json().catch(() => {
+                  return 0;
+                });
+              })
+            );
+          })
+          .then((data: DataElement[]) => {
+            data.forEach((element: DataElement) => {
+              filteredData = [
+                ...(filteredData || []),
+                ...(element.drinks || []),
+              ];
+            });
+            return filteredData;
+          })
+          .catch((err) => {
+            errorArray.push(err);
+          });
 
-      //         filtredData = [...filtredData, ...data.drinks];
-      //         filtredData = filtredData.filter((el) => {
-      //           const duplicate = seen.has(el.idDrink);
-      //           seen.add(el.idDrink);
-      //           return !duplicate;
-      //         });
-      //         // console.log('after duplicate delete', filtredData);
-      //       })
-      //       .catch((err) => errorArray.push(err));
-      //   });
-      //   console.log('filteredData', filtredData);
-      //   console.log('drinkArray', drinkArray);
+        filteredData = filteredData.filter((el: Drink) => {
+          const duplicate = seenDuplicate.has(el.idDrink);
+          seenDuplicate.add(el.idDrink);
+          return !duplicate;
+        });
 
-      //   drinkArray = [...drinkArray, ...filtredData];
-      // });
-      // console.log('Drink Array', drinkArray);
+        return filteredData;
+      });
 
-      drinkArray = choseDuplicates(drinkArray);
+      for (let i = 0; i < response.length; i++) {
+        drinkArray = drinkArray.concat(await response[i]);
+      }
+
+      // create array without duplicates
+      drinkArray = drinkArray.filter((el) => {
+        const duplicate = seen.has(el.idDrink);
+        seen.add(el.idDrink);
+        // create array of duplicates
+        if (duplicate) duplicateMap.set(el.idDrink, el);
+        return !duplicate;
+      });
+
+      const duplicateArray = Array.from(duplicateMap.values());
 
       const hasError =
         errorArray.length === phrasesArray.length * endPoints.length
           ? true
           : false;
-      return { drinkArray, hasError };
+
+      return { drinkArray, duplicateArray, hasError };
     },
     []
   );
 
-  const choseDuplicates = (duplicateArray: Drink[]): Drink[] => {
-    let counts: any = {};
-    let drinks: Drink[] = [];
-    console.log('duplicates?', duplicateArray);
-
-    duplicateArray.forEach((duplicateObject) => {
-      // if (counts[duplicateObject.idDrink]) {
-      counts[duplicateObject.idDrink] += 1;
-      console.log('counts contains:', counts);
-
-      // } else {
-      // counts[duplicateObject.idDrink] = 1;
-      // }
-      if (
-        counts[duplicateObject.idDrink] >= 2 &&
-        drinks.some((drink) => drink.idDrink !== duplicateObject.idDrink)
-      ) {
-        drinks.push(duplicateObject);
-      }
-    });
-    return drinks;
-  };
-
-  interface Data {
-    drinkArray: Object[];
-    hasError: boolean;
-  }
-
   useEffect(() => {
     const TimeOutID = setTimeout(() => {
-      const data = getDrinks(phrases, endPoints);
-
-      setDrinks(data.drinkArray);
-      setIsError(data.hasError);
-      setIsLoading(!data);
+      getDrinks(phrases, endPoints).then((res) => {
+        setDrinks(res.duplicateArray);
+        setIsError(res.hasError);
+        setIsLoading(false);
+      });
     }, 2000);
 
     return () => {
